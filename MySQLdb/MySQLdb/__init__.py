805b424895f5fe41fb47ad3937b546e373d83afb
adustman <adustman@9fc6cd9a-920d-0410-adcf-ac96716ed7e8>
2004-05-16 05:14:22 +0000

    Version Bump

--- MySQLdb/MySQLdb/__init__.py
+++ MySQLdb/MySQLdb/__init__.py
@@ -16,11 +16,11 @@ MySQLdb.converters module.
 __author__ = "Andy Dustman <andy@dustman.net>"
 __revision__ = """$Revision$"""[11:-2]
 version_info = (
+    1,
     0,
-    9,
-    3,
-    "beta",
-    3)
+    0,
+    "gamma",
+    1)
 if version_info[3] == "final": __version__ = "%d.%d.%d" % version_info[:3]
 else: __version__ = "%d.%d.%d%1.1s%d" % version_info[:5]
 
--- MySQLdb/_mysql.c
+++ MySQLdb/_mysql.c
@@ -1,5 +1,5 @@
-#define version_info "(0,9,3,'beta',3)"
-#define __version__ "0.9.3"
+#define version_info "(1,0,0,'gamma',1)"
+#define __version__ "1.0.0"
 /*
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
