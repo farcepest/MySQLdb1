ab06bb7a866f409b1b7a77ca4b21b97565870c57
adustman <adustman@9fc6cd9a-920d-0410-adcf-ac96716ed7e8>
2004-06-06 04:45:45 +0000

    1.0.0 (D-Day)

--- MySQLdb/setup.py
+++ MySQLdb/setup.py
@@ -18,7 +18,7 @@ embedded_server = (mysqlclient == 'mysqld')
 name = "MySQL-%s" % os.path.basename(sys.executable)
 if embedded_server:
     name = name + "-embedded"
-version = "1.0.0c2"
+version = "1.0.0"
 
 # include files and library locations should cover most platforms
 include_dirs = [
